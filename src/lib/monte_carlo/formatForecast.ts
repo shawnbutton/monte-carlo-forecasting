c1df5d69d2124927263308aa1ae583135be62312
Shawn Button <shawn.button@bell.ca>
2024-05-27 11:15:57 -0400

    default to week period

--- src/lib/monte_carlo/formatForecast.ts
+++ src/lib/monte_carlo/formatForecast.ts
@@ -1,24 +1,23 @@
 import { addDays, addMonths, isValid, lightFormat, parse } from 'date-fns'
 
-type Period = 'week' | 'sprint' | 'month'
-
 const dateToString = (data: Date) => lightFormat(data, 'yyyy-MM-dd')
 
 const addDaysToStart = (startDate: Date, daysToAdd: number) => (periodOn: number) => addDays(startDate, periodOn * daysToAdd)
 const addMonthsToStart = (startDate: Date) => (periodOn: number) => addMonths(startDate, periodOn)
 
-const getPeriodAdder = (startDate: Date, period: Period) => {
+const getPeriodAdder = (startDate: Date, period: string) => {
 	switch (period) {
-		case 'week':
-			return addDaysToStart(startDate, 7)
 		case 'sprint':
 			return addDaysToStart(startDate, 14)
 		case 'month':
 			return addMonthsToStart(startDate)
+		default:
+			// week
+			return addDaysToStart(startDate, 7)
 	}
 }
 
-export const formatForecast = (weeks: number[][], startDateString: string, period: Period) => {
+export const formatForecast = (weeks: number[][], startDateString: string, period: string) => {
 	let startDate = parse(startDateString, 'yyyy-MM-dd', new Date())
 	if (!isValid(startDate)) startDate = new Date()
 
