bd117527ff3c598a25721963230ca8fc91dc22c8
Shawn Button <shawn.button@bell.ca>
2024-05-28 13:49:20 -0400

    use snapshot test for formatting data

--- src/lib/monte_carlo/formatForecast.test.ts
+++ src/lib/monte_carlo/formatForecast.test.ts
@@ -4,13 +4,11 @@ import { formatForecast } from '$lib/monte_carlo/formatForecast'
 const arrayFilledWith = (size: number, value: number | boolean) => Array(size).fill(value)
 
 describe('forecast formatter', () => {
-	it('', () => {
+	it('formats with all percentages in weeks', () => {
 
 		const weeks = [arrayFilledWith(99, 1), arrayFilledWith(99, 2)]
 		const result = formatForecast(weeks, '2024-05-05', 'week', arrayFilledWith(19, true))
 
-		expect(result).toEqual(`"Date", 5%, 10%, 15%, 20%, 25%, 30%, 35%, 40%, 45%, 50%, 55%, 60%, 65%, 70%, 75%, 80%, 85%, 90%, 95%
-2024-05-05, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-2024-05-12, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2`)
+		expect(result).toMatchSnapshot()
 	})
 })
